d231443d3e57cc26012777550fe1705c67b5e2d9
sergioalberto <sergioalbertogq@gmail.com>
2013-09-11 19:07:40 -0700

    agregue nuevas funcionalidades para reproducir .ogg

--- reproducir_mp3.c
+++ reproducir_mp3.c
@@ -25,37 +25,113 @@
 #include "reproducir_mp3.h"
 
 void play(char *name){
-	/* create elements */
-	pipeline = gst_pipeline_new ("my_pipeline"); // Create a new pipeline with the given name.
 
-	filesrc  = gst_element_factory_make ("filesrc", "my_filesource");
-	decoder  = gst_element_factory_make ("mad", "my_decoder");
+	/* Create gstreamer elements */
+	pipeline = gst_pipeline_new ("audio-player");
+	source   = gst_element_factory_make ("filesrc",       "file-source");
+	demuxer  = gst_element_factory_make ("decodebin",      "ogg-demuxer");
+	decoder  = gst_element_factory_make ("audioresample",     "vorbis-decoder");
+	conv     = gst_element_factory_make ("audioconvert",  "converter");
+	sink     = gst_element_factory_make ("autoaudiosink", "audio-output");
 
-	sink     = gst_element_factory_make ("pulsesink", "audiosink");
-
-	g_object_set (G_OBJECT (filesrc), "location", name, NULL);
-
-	gst_bin_add_many (GST_BIN (pipeline), filesrc, decoder, sink, NULL);
-
-	/* link everything together */
-	if (!gst_element_link_many (filesrc, decoder, sink, NULL)) {
-		g_print ("Failed to link one or more elements!\n");
+	if (!pipeline || !source || !demuxer || !decoder || !conv || !sink) {
+		g_printerr ("One element could not be created. Exiting.\n");
 		exit(0);
 	}
 
-	/* run */
-  	ret = gst_element_set_state (pipeline, GST_STATE_PLAYING);
-	if (ret == GST_STATE_CHANGE_FAILURE) {
-		g_print ("Failed to start up pipeline!\n");
-		exit(0);
-	}
+	/* Set up the pipeline */
 
+	/* we set the input filename to the source element */
+	g_object_set (G_OBJECT (source), "location", name, NULL);
+
+	/* we add a message handler */
+	bus = gst_pipeline_get_bus (GST_PIPELINE (pipeline));
+	bus_watch_id = gst_bus_add_watch (bus, bus_call, loop);
+	gst_object_unref (bus);
+
+	/* we add all elements into the pipeline */
+	/* file-source | ogg-demuxer | vorbis-decoder | converter | alsa-output */
+	gst_bin_add_many (GST_BIN (pipeline), source, demuxer, decoder, conv, sink, NULL);
+
+	/* we link the elements together */
+	/* file-source -> ogg-demuxer ~> vorbis-decoder -> converter -> alsa-output */
+	gst_element_link (source, demuxer);
+	gst_element_link_many (decoder, conv, sink, NULL);
+	g_signal_connect (demuxer, "pad-added", G_CALLBACK (on_pad_added), decoder);
+
+	/* note that the demuxer will be linked to the decoder dynamically.
+	The reason is that Ogg may contain various streams (for example
+	audio and video). The source pad(s) will be created at run time,
+	by the demuxer when it detects the amount and nature of streams.
+	Therefore we connect a callback function which will be executed
+	when the "pad-added" is emitted.*/
+
+
+	/* Set the pipeline to "playing" state*/
+	g_print ("Now playing: %s\n", name);
+	gst_element_set_state (pipeline, GST_STATE_PLAYING);
+
+
+	/* Iterate */
+	g_print ("Running...\n");
 	g_main_loop_run (loop);
 
-	/* clean up */
+
+	/* Out of the main loop, clean up nicely */
+	g_print ("Returned, stopping playback\n");
 	gst_element_set_state (pipeline, GST_STATE_NULL);
-	gst_object_unref (pipeline);
+
+	g_print ("Deleting pipeline\n");
+	gst_object_unref (GST_OBJECT (pipeline));
+	g_source_remove (bus_watch_id);
 	g_main_loop_unref (loop);
+
+}
+
+static void on_pad_added (GstElement *element, GstPad *pad, gpointer data) {
+
+  GstPad *sinkpad;
+  GstElement *decoder = (GstElement *) data;
+
+  /* We can now link this pad with the vorbis-decoder sink pad */
+  g_print ("Dynamic pad created, linking demuxer/decoder\n");
+
+  sinkpad = gst_element_get_static_pad (decoder, "sink");
+
+  gst_pad_link (pad, sinkpad);
+
+  gst_object_unref (sinkpad);
+}
+
+static gboolean bus_call (GstBus *bus, GstMessage *msg, gpointer data) {
+
+  GMainLoop *loop = (GMainLoop *) data;
+
+  switch (GST_MESSAGE_TYPE (msg)) {
+
+    case GST_MESSAGE_EOS:
+      g_print ("End of stream\n");
+      g_main_loop_quit (loop);
+      break;
+
+    case GST_MESSAGE_ERROR: {
+      gchar  *debug;
+      GError *error;
+
+      gst_message_parse_error (msg, &error, &debug);
+      g_free (debug);
+
+      g_printerr ("Error: %s\n", error->message);
+      g_error_free (error);
+
+      g_main_loop_quit (loop);
+      break;
+    }
+    default:
+      break;
+  }
+
+  return TRUE;
 }
 
 int main (int argc, gchar *argv[]) {
